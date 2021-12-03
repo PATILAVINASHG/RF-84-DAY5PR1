4f41aba0e39680e3ee8f40421b4e928367ee9362
avinashpatil <patilavinash223@gmail.com>
2021-12-03 17:43:13 +0530

    add swapnumber file

---
+++ swapprgm/Swap.java
@@ -0,0 +1,18 @@
+package swapprgm;
+
+public class Swap {
+		
+		public static void main(String[] args) {
+			
+		int a = 12;
+		int b = 14;
+		
+		System.out.println("Before swaping the : " + a +"\n" + b);
+		
+		int c = a ;
+		a = b;
+		b = c;
+		System.out.println("after swaping the : " + a + "\n" + b);
+		}
+		
+}
